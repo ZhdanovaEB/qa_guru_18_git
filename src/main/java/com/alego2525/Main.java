4cf3fa735a1357db4eda7bcd2868623a3fa7dd1a
Жданова Екатерина Борисовна (4079982) <ebzhdanova@vtb.ru>
2023-02-17 18:23:23 +0700

    Изменено имя

--- src/main/java/com/alego2525/Main.java
+++ src/main/java/com/alego2525/Main.java
@@ -1,7 +1,7 @@
 package com.alego2525;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String... args) {
+        System.out.println("Hello Katya!");
     }
 }

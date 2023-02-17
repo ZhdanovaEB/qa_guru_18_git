8a9aa317e445d5561027ccadec38a077a4fc6fac
Екатерина Жданова <57565237+ZhdanovaEB@users.noreply.github.com>
2023-02-17 18:26:30 +0700

    Update Main.java

--- src/main/java/com/alego2525/Main.java
+++ src/main/java/com/alego2525/Main.java
@@ -2,6 +2,6 @@ package com.alego2525;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Ekaterina!");
     }
 }
